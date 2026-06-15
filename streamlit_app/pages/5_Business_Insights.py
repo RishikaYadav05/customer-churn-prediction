2da666533d8304ab42d691d718d6be99d36ad665
RishikaYadav05 <namity630@gmail.com>
2026-06-15 19:11:18 +0530

    Create 5_Business_Insights.py

---
+++ streamlit_app/pages/5_Business_Insights.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("💡 Business Insights")
+
+st.markdown("""
+### Key Recommendations
+
+1. Focus on customers with low tenure.
+
+2. Encourage long-term contracts.
+
+3. Monitor customers with high monthly charges.
+
+4. Reward loyal customers.
+
+5. Create retention campaigns for high-risk users.
+""")
