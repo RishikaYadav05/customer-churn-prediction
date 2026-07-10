d6a92e6584eca72960f7d78803c234d45619685d
RishikaYadav05 <namity630@gmail.com>
2026-07-10 17:34:35 +0530

    Update 3_Model_Comparison.py

--- streamlit_app/pages/3_Model_Comparison.py
+++ streamlit_app/pages/3_Model_Comparison.py
@@ -1,14 +1,27 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+
+# ------------------------
+# Project Base Directory
+# ------------------------
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+FEATURE_IMAGE = BASE_DIR / "streamlit_app" / "feature_importance.png"
+
+# ------------------------
+# Model Results
+# ------------------------
 
 results = pd.DataFrame({
-    "Model":[
+    "Model": [
         "Logistic Regression",
         "Decision Tree",
         "Random Forest",
         "KNN"
     ],
-    "Accuracy":[
+    "Accuracy": [
         0.7594,
         0.7331,
         0.7828,
@@ -18,8 +31,17 @@ results = pd.DataFrame({
 
 st.title("🏆 Model Comparison")
 
+st.subheader("Model Accuracy")
+
 st.dataframe(results)
 
 st.bar_chart(
     results.set_index("Model")
+)
+
+st.subheader("Feature Importance")
+
+st.image(
+    FEATURE_IMAGE,
+    use_container_width=True
 )
