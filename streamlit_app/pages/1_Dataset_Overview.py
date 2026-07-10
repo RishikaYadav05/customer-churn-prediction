c36d367decb5c732d9147f82d06a9dbb08b19753
RishikaYadav05 <namity630@gmail.com>
2026-07-10 17:32:46 +0530

    Update 1_Dataset_Overview.py

--- streamlit_app/pages/1_Dataset_Overview.py
+++ streamlit_app/pages/1_Dataset_Overview.py
@@ -2,27 +2,19 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
-# ------------------------
-# Load Dataset
-# ------------------------
-
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 DATA_PATH = BASE_DIR / "data" / "WA_Fn-UseC_-Telco-Customer-Churn.csv"
 
 df = pd.read_csv(DATA_PATH)
 
-# ------------------------
-# Dataset Overview
-# ------------------------
-
 st.title("📊 Dataset Overview")
 
-st.write("### Dataset Shape")
+st.subheader("Dataset Shape")
 st.write(df.shape)
 
-st.write("### First 5 Rows")
+st.subheader("First 5 Rows")
 st.dataframe(df.head())
 
-st.write("### Statistical Summary")
+st.subheader("Statistical Summary")
 st.dataframe(df.describe())
