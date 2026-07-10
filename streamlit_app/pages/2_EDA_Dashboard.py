6e0956a9e8c9da758fc419cffc55373d34a110f2
RishikaYadav05 <namity630@gmail.com>
2026-07-10 17:43:48 +0530

    Update 2_EDA_Dashboard.py

--- streamlit_app/pages/2_EDA_Dashboard.py
+++ streamlit_app/pages/2_EDA_Dashboard.py
@@ -4,26 +4,26 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# Base directory of the project
+# Project root directory
 BASE_DIR = Path(__file__).resolve().parents[2]
 
-# Dataset path
 DATA_PATH = BASE_DIR / "data" / "WA_Fn-UseC_-Telco-Customer-Churn.csv"
-
-# PCA image path
 PCA_IMAGE = BASE_DIR / "streamlit_app" / "pca_visualization.png"
 
-# Load dataset
 df = pd.read_csv(DATA_PATH)
 
 st.title("📈 EDA Dashboard")
 
-# Churn Distribution
-fig, ax = plt.subplots(figsize=(6, 4))
-sns.countplot(x="Churn", data=df, ax=ax)
-ax.set_title("Customer Churn Distribution")
+fig, ax = plt.subplots(figsize=(6,4))
+
+sns.countplot(
+    x="Churn",
+    data=df,
+    ax=ax
+)
+
 st.pyplot(fig)
 
-# PCA Visualization
 st.subheader("PCA Visualization")
+
 st.image(PCA_IMAGE, use_container_width=True)
